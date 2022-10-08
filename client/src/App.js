779123c45342fa3679554dd76a0a0f197538d777
azariahgarrido <azariahdanielgarrido@gmail.com>
2022-10-08 12:02:50 -0700

    Added a navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import Homepage from './pages/Homepage';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import Homepage from './pages/Homepage';
+import Header from './components/Header';
+import QuizOne from './pages/QuizOne';
+import QuizTwo from './pages/QuizTwo';
 
 const client = new ApolloClient({
   request: operation => {
@@ -20,9 +23,25 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div className='mainHome'><Homepage /></div>
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <Routes>
+          {/* <Route 
+                  path="/" 
+                  element={<Homepage />} 
+          /> */}
+          <Route 
+                  path="/QuizOne" 
+                  element={<QuizOne />}
+          />
+          <Route 
+                  path="/QuizTwo" 
+                  element={<QuizTwo />}
+          />
+          </Routes>
+        </Router>
+      </ApolloProvider>
   );
 }
 
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,5 +1,25 @@
-export default function Homepage() {
-    return(
-        <button className = "home">START</button>
-    )
-}
+// import { useNavigate } from 'react-router-dom';
+
+
+// export default function Homepage() {
+//     const navigate = useNavigate();
+    
+//     const navigateToQuizOne = () => {
+//         navigate('/QuizOne');
+//     };
+//     const navigateToQuizTwo = () => {
+//         navigate('/QuizTwo');
+//     };
+
+//     return(
+//         <main>
+//             <div>
+//                 <button className = "home" onClick={navigateToQuizOne}>Quiz One</button>
+//                 <button className = "home" onClick={navigateToQuizTwo}>Quiz Two</button>
+//             </div>
+//             <div>
+//                 <button>Make your own Quiz</button>
+//             </div>
+//         </main>
+//     )
+// }
