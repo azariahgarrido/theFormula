59c2ed12774e33a98fc2f5213b2f69f0dde5b2bb
Azariah Garrido <azariahdanielgarrido@gmail.com>
2022-10-15 20:37:16 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://azariahgarrido:softwareEngineer4869@cluster0.vcm7lvv.mongodb.net/theFormula?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/theFormula', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
